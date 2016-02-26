1051c41831eb326ad9d3720145cc8fe6204dffb4
hevan <hevanwang@gmail.com>
2016-02-26 11:53:44 +0800

    update clients

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,37 +1,88 @@
-CREATE TABLE user (
-  id int not null AUTO_INCREMENT  PRIMARY KEY,
-  username VARCHAR(50) NOT NULL,
-  email VARCHAR(50),
-  password VARCHAR(500),
-  activated BOOLEAN DEFAULT FALSE,
-  activationkey VARCHAR(50) DEFAULT NULL,
-  resetpasswordkey VARCHAR(50) DEFAULT NULL
-);
+CREATE TABLE `authority` (
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE authority (
-  name VARCHAR(50) NOT NULL PRIMARY KEY
-);
+CREATE TABLE `ClientDetails` (
+  `appId` varchar(256) NOT NULL,
+  `resourceIds` varchar(256) DEFAULT NULL,
+  `appSecret` varchar(256) DEFAULT NULL,
+  `scope` varchar(256) DEFAULT NULL,
+  `grantTypes` varchar(256) DEFAULT NULL,
+  `redirectUrl` varchar(256) DEFAULT NULL,
+  `authorities` varchar(256) DEFAULT NULL,
+  `access_token_validity` int(11) DEFAULT NULL,
+  `refresh_token_validity` int(11) DEFAULT NULL,
+  `additionalInformation` blob,
+  `autoApproveScopes` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`appId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE user_authority (
-    id int NOT NULL,
-    authority VARCHAR(50) NOT NULL,
-    FOREIGN KEY (id) REFERENCES user (id),
-    FOREIGN KEY (authority) REFERENCES authority (name),
-    UNIQUE INDEX user_authority_idx_1 (id, authority)
-);
+CREATE TABLE `oauth_access_token` (
+  `token_id` varchar(256) DEFAULT NULL,
+  `token` blob,
+  `authentication_id` varchar(256) DEFAULT NULL,
+  `user_name` varchar(256) DEFAULT NULL,
+  `client_id` varchar(256) DEFAULT NULL,
+  `authentication` blob,
+  `refresh_token` varchar(256) DEFAULT NULL,
+  KEY `client_id_index` (`client_id`(256))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE oauth_access_token (
-  token_id VARCHAR(256) DEFAULT NULL,
-  token BLOB,
-  authentication_id VARCHAR(256) DEFAULT NULL,
-  user_name VARCHAR(256) DEFAULT NULL,
-  client_id VARCHAR(256) DEFAULT NULL,
-  authentication BLOB,
-  refresh_token VARCHAR(256) DEFAULT NULL
-);
+CREATE TABLE `oauth_approvals` (
+  `userId` varchar(256) DEFAULT NULL,
+  `clientId` varchar(256) DEFAULT NULL,
+  `scope` varchar(256) DEFAULT NULL,
+  `status` varchar(10) DEFAULT NULL,
+  `expiresAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `lastModifiedAt` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE oauth_refresh_token (
-  token_id VARCHAR(256) DEFAULT NULL,
-  token BLOB,
-  authentication BLOB
-);
+CREATE TABLE `oauth_client_details` (
+  `client_id` varchar(256) NOT NULL,
+  `resource_ids` varchar(256) DEFAULT NULL,
+  `client_secret` varchar(256) DEFAULT NULL,
+  `scope` varchar(256) DEFAULT NULL,
+  `authorized_grant_types` varchar(256) DEFAULT NULL,
+  `web_server_redirect_uri` varchar(256) DEFAULT NULL,
+  `authorities` varchar(256) DEFAULT NULL,
+  `access_token_validity` int(11) DEFAULT NULL,
+  `refresh_token_validity` int(11) DEFAULT NULL,
+  `additional_information` text,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `archived` tinyint(1) DEFAULT '0',
+  `trusted` tinyint(1) DEFAULT '0',
+  `autoapprove` varchar(256) DEFAULT 'false',
+  PRIMARY KEY (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `oauth_code` (
+  `code` varchar(256) DEFAULT NULL,
+  `authentication` blob
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `oauth_refresh_token` (
+  `token_id` varchar(256) DEFAULT NULL,
+  `token` blob,
+  `authentication` blob
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `password` varchar(500) DEFAULT NULL,
+  `activated` tinyint(1) DEFAULT '0',
+  `activationkey` varchar(50) DEFAULT NULL,
+  `resetpasswordkey` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user_authority` (
+  `id` int(11) NOT NULL,
+  `authority` varchar(50) NOT NULL,
+  UNIQUE KEY `user_authority_idx_1` (`id`,`authority`),
+  KEY `authority` (`authority`),
+  CONSTRAINT `user_authority_ibfk_1` FOREIGN KEY (`id`) REFERENCES `user` (`id`),
+  CONSTRAINT `user_authority_ibfk_2` FOREIGN KEY (`authority`) REFERENCES `authority` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
