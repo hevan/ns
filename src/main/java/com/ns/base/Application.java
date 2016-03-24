93be4406ed72573295ec25cc70d4e6345af9bda0
hevanwang <hevanwang@gmail.com>
2016-03-24 11:07:30 +0800

    update new date

--- src/main/java/com/ns/base/Application.java
+++ src/main/java/com/ns/base/Application.java
@@ -1,23 +1,178 @@
 package com.ns.base;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.bind.RelaxedPropertyResolver;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@Configuration
-@EnableAutoConfiguration
+import com.ns.base.security.Authorities;
+import com.ns.base.security.CustomAuthenticationEntryPoint;
+import com.ns.base.security.CustomLogoutSuccessHandler;
+
+@SpringBootApplication
+
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+//@EnableAutoConfiguration
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class Application extends SpringBootServletInitializer {
 
 	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
-
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+	
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
+  
+    
+    @EnableWebSecurity
+    @Order(-20)
+    public static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+        @Autowired
+        private UserDetailsService userDetailsService;
+
+        @Autowired
+    	private AuthenticationManager authenticationManager;
+        
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new StandardPasswordEncoder();
+        }
+
+        @Override
+    	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    		auth.parentAuthenticationManager(authenticationManager).userDetailsService(userDetailsService);
+    	}
+
+        
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+
+            web
+                    .ignoring()
+                    .antMatchers("/register")
+                    .antMatchers("/activate")
+                    .antMatchers("/lostpassword")
+                    .antMatchers("/resetpassword")
+                    .antMatchers("/hello");
+
+        }
+        
+        /*
+        @EnableGlobalMethodSecurity(prePostEnabled = true)
+        @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+        private static class GlobalSecurityConfiguration extends GlobalMethodSecurityConfiguration {
+            @Override
+            protected MethodSecurityExpressionHandler createExpressionHandler() {
+                return new OAuth2MethodSecurityExpressionHandler();
+            }
+
+        }*/
+
+    }
+
+    
+    
+    @EnableAuthorizationServer
+    protected static class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter implements EnvironmentAware {
+
+        private static final String ENV_OAUTH = "authentication.oauth.";
+        private static final String PROP_CLIENTID = "clientid";
+        private static final String PROP_SECRET = "secret";
+        private static final String PROP_TOKEN_VALIDITY_SECONDS = "tokenValidityInSeconds";
+
+        private RelaxedPropertyResolver propertyResolver;
+
+        
+        @Autowired
+        private DataSource dataSource;
+
+        @Bean
+        public TokenStore tokenStore() {
+            return new JdbcTokenStore(dataSource);
+        }
+        /*
+        @Bean
+		public JwtAccessTokenConverter jwtAccessTokenConverter() {
+			JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+			KeyPair keyPair = new KeyStoreKeyFactory(new ClassPathResource("keystore.jks"), "foobar".toCharArray())
+					.getKeyPair("test");
+			converter.setKeyPair(keyPair);
+			return converter;
+		}*/
+
+        @Autowired
+        private AuthenticationManager authenticationManager;
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints)
+                throws Exception {
+            endpoints
+                    .tokenStore(tokenStore())
+                    .authenticationManager(authenticationManager);
+                    //.authenticationManager(authenticationManager).accessTokenConverter(jwtAccessTokenConverter());
+        }
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients
+            .inMemory()
+                    .withClient(propertyResolver.getProperty(PROP_CLIENTID))
+                    .scopes("read", "write","openid")
+                    .authorities(Authorities.ROLE_ADMIN.name(), Authorities.ROLE_USER.name())
+                    .authorizedGrantTypes("authorization_code","refresh_token","password")
+                    .secret(propertyResolver.getProperty(PROP_SECRET))
+                    .accessTokenValiditySeconds(propertyResolver.getProperty(PROP_TOKEN_VALIDITY_SECONDS, Integer.class, 1800));
+        }
+        
+        /*
+        @Override
+		public void configure(AuthorizationServerSecurityConfigurer oauthServer)
+				throws Exception {
+			oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+		}
+		*/
+
+        @Override
+        public void setEnvironment(Environment environment) {
+            this.propertyResolver = new RelaxedPropertyResolver(environment, ENV_OAUTH);
+        }
+
+    }
+
+    
+    
+    
 }
 
--- src/main/java/com/ns/base/base/OAuth2Configuration.java
+++ src/main/java/com/ns/base/base/OAuth2Configuration.java
@@ -1,7 +1,9 @@
+/*
 package com.ns.base.base;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.bind.RelaxedPropertyResolver;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
@@ -9,13 +11,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -24,12 +27,12 @@ import com.ns.base.security.Authorities;
 import com.ns.base.security.CustomAuthenticationEntryPoint;
 import com.ns.base.security.CustomLogoutSuccessHandler;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class OAuth2Configuration {
 
-    @Configuration
+	private static final String RESOURCE_ID = "ns";
+	
+	
     @EnableResourceServer
     protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 
@@ -40,31 +43,43 @@ public class OAuth2Configuration {
         private CustomLogoutSuccessHandler customLogoutSuccessHandler;
 
         @Override
+		public void configure(ResourceServerSecurityConfigurer resources) {
+			// @formatter:off
+			resources
+				.resourceId(RESOURCE_ID);
+			// @formatter:on
+		}
+        
+        
+        @Override
         public void configure(HttpSecurity http) throws Exception {
 
-            http
-                    .exceptionHandling()
-                    .authenticationEntryPoint(customAuthenticationEntryPoint)
-                    .and()
-                    .logout()
-                    .logoutUrl("/oauth/logout")
-                    .logoutSuccessHandler(customLogoutSuccessHandler)
-                    .and()
-                    .csrf()
-                    .requireCsrfProtectionMatcher(new AntPathRequestMatcher("/oauth/authorize"))
-                    .disable()
-                    .headers()
-                    .frameOptions().disable()
-                    .and()
-                    .authorizeRequests()
-                    .antMatchers("/hello/").permitAll()
-                    .antMatchers("/secure/**").authenticated();
+        	 http
+             .exceptionHandling()
+             .authenticationEntryPoint(customAuthenticationEntryPoint)
+             .and()
+             .logout()
+             .logoutUrl("/oauth/logout")
+             .logoutSuccessHandler(customLogoutSuccessHandler)
+             .and()
+             .csrf()
+             .requireCsrfProtectionMatcher(new AntPathRequestMatcher("/oauth/authorize"))
+             .disable()
+             .headers()
+             .frameOptions().disable()
+             .and()
+             .authorizeRequests()
+             .antMatchers("/hello/").permitAll()
+             .antMatchers("/secure/**").authenticated();
 
         }
 
-    }
 
-    @Configuration
+
+///MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnGp/Q5lh0P8nPL21oMMrt2RrkT9AW5jgYwLfSUnJVc9G6uR3cXRRDCjHqWU5WYwivcF180A6CWp/ireQFFBNowgc5XaA0kPpzEtgsA5YsNX7iSnUibB004iBTfU9hZ2Rbsc8cWqynT0RyN4TP1RYVSeVKvMQk4GT1r7JCEC+TNu1ELmbNwMQyzKjsfBXyIOCFU/E94ktvsTZUHF4Oq44DBylCDsS1k7/sfZC2G5EU7Oz0mhG8
+    }
+    
+    
     @EnableAuthorizationServer
     protected static class AuthorizationServerConfiguration extends AuthorizationServerConfigurerAdapter implements EnvironmentAware {
 
@@ -75,6 +90,7 @@ public class OAuth2Configuration {
 
         private RelaxedPropertyResolver propertyResolver;
 
+        
         @Autowired
         private DataSource dataSource;
 
@@ -82,9 +98,17 @@ public class OAuth2Configuration {
         public TokenStore tokenStore() {
             return new JdbcTokenStore(dataSource);
         }
+        
+        @Bean
+		public JwtAccessTokenConverter jwtAccessTokenConverter() {
+			JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+			KeyPair keyPair = new KeyStoreKeyFactory(new ClassPathResource("keystore.jks"), "foobar".toCharArray())
+					.getKeyPair("test");
+			converter.setKeyPair(keyPair);
+			return converter;
+		}
 
         @Autowired
-        @Qualifier("authenticationManagerBean")
         private AuthenticationManager authenticationManager;
 
         @Override
@@ -93,19 +117,28 @@ public class OAuth2Configuration {
             endpoints
                     .tokenStore(tokenStore())
                     .authenticationManager(authenticationManager);
+                    //.authenticationManager(authenticationManager).accessTokenConverter(jwtAccessTokenConverter());
         }
 
         @Override
         public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
             clients
-                    .jdbc(dataSource)
+            .inMemory()
                     .withClient(propertyResolver.getProperty(PROP_CLIENTID))
-                    .scopes("read", "write")
+                    .scopes("read", "write","openid")
                     .authorities(Authorities.ROLE_ADMIN.name(), Authorities.ROLE_USER.name())
-                    .authorizedGrantTypes("password", "refresh_token")
+                    .authorizedGrantTypes("authorization_code","refresh_token","password")
                     .secret(propertyResolver.getProperty(PROP_SECRET))
                     .accessTokenValiditySeconds(propertyResolver.getProperty(PROP_TOKEN_VALIDITY_SECONDS, Integer.class, 1800));
         }
+        
+        
+        @Override
+		public void configure(AuthorizationServerSecurityConfigurer oauthServer)
+				throws Exception {
+			oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
+		}
+		
 
         @Override
         public void setEnvironment(Environment environment) {
@@ -115,3 +148,4 @@ public class OAuth2Configuration {
     }
 
 }
+*/
